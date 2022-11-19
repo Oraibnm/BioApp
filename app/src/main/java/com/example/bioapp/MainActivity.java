22160eb125819ba8247ea1788c423b940e8b7c87
Oraibnm <oraibnidal@gmail.com>
2022-11-19 21:47:25 +0200

    test3 commit

--- app/src/main/java/com/example/bioapp/MainActivity.java
+++ app/src/main/java/com/example/bioapp/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.bioapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //first declare if i have buttons , textViews or EditTexts
     private Button btn ;
     private TextView textView;
     private EditText editText;
@@ -20,7 +22,18 @@ public class MainActivity extends AppCompatActivity {
     }
     private  void test () {
      btn = findViewById(R.id.Addbutton);
-     textView = findViewById(R.id.textView);
-     editText = findViewById(R.id.editText);
+     textView = findViewById(R.id.txtHobbies);
+     editText = findViewById(R.id.editHobbies);
+
+     btn.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             String hobbies = editText.getText().toString();
+             if (!hobbies.isEmpty()){
+                 textView.setText(hobbies);
+                 textView.setVisibility(View.VISIBLE);
+             }
+         }
+     });
     }
 }
